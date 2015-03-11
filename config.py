4a235ceff24a6b423f17b3d68407cef089feff8e
Ben Debebe <bdebebe@andrew.cmu.edu>
2015-03-11 12:47:21 -0400

    db

--- config.py
+++ config.py
@@ -1,2 +1,27 @@
+import os
+
 SQLALCHEMY_DATABASE_URI = "postgresql://bendebebe:chelsea@localhost/tally-up"
 SECRET_KEY = '48205hrjf0238'
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
